b6eabc128984d28ae6b713cdad3fa2678182b05a
sion-k <ygonepiece@naver.com>
2024-10-11 18:39:08 +0900

    캐싱 버그 수정

--- practice.js
+++ practice.js
@@ -65,41 +65,28 @@ async function display() {
     // Loop through each row and add a new cell
     rows.forEach((row, index) => {
         const newCell = document.createElement('td');
-        const select = document.createElement('select');
+        const input = document.createElement('input');
+        // add input to textoverflow
+        input.style.textOverflow = 'ellipsis';
+        input.setAttribute('list', 'contest-options');
 
-        const options = ['없음', ...Object.keys(contest_name_id_dict)];
+        const datalist = document.createElement('datalist');
+        datalist.id = 'contest-options';
+
+        const options = [...Object.keys(contest_name_id_dict)];
         options.forEach((option) => {
             const optionElement = document.createElement('option');
             optionElement.value = option;
-            optionElement.textContent = option;
-            select.appendChild(optionElement);
+            datalist.appendChild(optionElement);
         });
 
         const [groupId, practiceId] = parse_group_practice_id(row);
         const practice = get_practice(groupId, practiceId);
 
-        select.value = practice['contest_name'] || options[0]; // Default to the first option
-        /*
-        <tr class="">
-            <td><a href="/group/practice/view/13872/65">2022 Southeast USA Regional Programming Contest DIvision 2</a></td>
-            <td><span data-timestamp="1727757000" class="update-local-time" data-remove-second="true">2024년 10월 1일 13:30</span>
-            </td>
-            <td><span data-timestamp="1727767800" class="update-local-time" data-remove-second="true">2024년 10월 1일 16:30</span>
-            </td>
-            <td>종료</td>
-            <td><a href="/status?group_id=13872&amp;practice_id=65">채점 현황</a></td>
-            <td><a href="/group/practice/edit/13872/65">수정</a></td>
-            <td><select>
-                    <option value="없음">없음</option>
-                    <option value="2022 Southeast USA Regional Programming Contest Division 2">2022 Southeast USA Regional
-                        Programming Contest Division 2</option>
-                    <option value="2021 Southeast USA Regional Programming Contest Division 2">2021 Southeast USA Regional
-                        Programming Contest Division 2</option>
-                    <option value="2020 Southeast USA Regional Programming Contest Division 2">2020 Southeast USA Regional
-                        Programming Contest Division 2</option>
-                </select></td>
-        </tr>
-        */
+        input.value = practice['contest_name'] || '';
+
+        newCell.appendChild(input);
+        newCell.appendChild(datalist);
         const practice_start =
             row.children[1].children[0].getAttribute('data-timestamp');
         const practice_end =
@@ -107,23 +94,25 @@ async function display() {
 
         practice['practice_start'] = parseInt(practice_start);
         practice['practice_end'] = parseInt(practice_end);
+        delete practice['competition_scoreboard'];
+
         localStorage.setItem(
             `iguana/group/${groupId}/practice/${practiceId}`,
             JSON.stringify(practice)
         );
 
         // Store data in localStorage when the selection changes
-        select.addEventListener('change', () => {
-            practice['contest_id'] = contest_name_id_dict[select.value];
+        input.addEventListener('change', () => {
+            practice['contest_id'] = contest_name_id_dict[input.value];
             practice['contest_name'] =
-                select.value === '없음' ? undefined : select.value;
+                input.value === '' ? undefined : input.value;
+            delete practice['competition_scoreboard'];
             localStorage.setItem(
                 `iguana/group/${groupId}/practice/${practiceId}`,
                 JSON.stringify(practice)
             );
         });
 
-        newCell.appendChild(select);
         row.appendChild(newCell);
     });
 }
